11e671c5e0579099b0cfed41c76879f2f56ee621
rkgupta1298 <rkgupta1298@gmail.com>
2020-09-09 01:25:32 +0530

    Loop added for todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [todos,setTodos] = useState(['Get done with React','Get done with Springboot'])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <h1>Hello Chiti Bhagi Suman</h1>
+        <input />
+        <button> Add Todo </button>
+        <ul>
+          {todos.map(todo => (
+            <li>{todo}</li>
+          ))}
+        </ul>
+.    </div>
   );
 }
 
